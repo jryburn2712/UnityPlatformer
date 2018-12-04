c3a08d6d37ffe634b01d7804652963c882a18c9e
aleuts <myauntcoco@gmail.com>
2018-12-04 17:10:58 +0000

    Merge offline attack animation and scripts

---
+++ Assets/scripts/Player/input/commands/AttackCommand.cs
@@ -0,0 +1,14 @@
+﻿class AttackCommand : Command
+{
+    public AttackState attackState;
+
+    public AttackCommand()
+    {
+        attackState = new AttackState();
+    }
+
+    public override void Execute(Player player)
+    {
+        player.State.SetState(player, attackState);
+    }
+}
---
+++ Assets/scripts/Player/state/AttackState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+class AttackState : State
+{
+    public override void OnStateEnter(Player player)
+    {
+        base.OnStateEnter(player);
+
+        //Start Attack Animation. Check to make sure it's not already playing.
+        if (!player.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("male_attack"))
+        {
+            player.playerAnimator.Play("male_attack");
+            //player.playerAnimator.SetTrigger("male_attack");
+        }
+    }
+
+    public override void Tick(Player player)
+    {
+        //Attack(player);
+    }
+
+    private void Attack(Player player)
+    {
+        
+    }
+}
