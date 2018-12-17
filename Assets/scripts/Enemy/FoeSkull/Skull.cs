ee4b89a284cae1a066676d492c56fbfdaa7a42ae
James Ryburn <jpryburn@gmail.com>
2018-12-16 19:45:02 -0500

    Added Skull enemy prefab (with animation & bouncy material). No hit detection yet.

---
+++ Assets/scripts/Enemy/FoeSkull/Skull.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Skull : MonoBehaviour {
+
+    private Rigidbody2D cachedRigidBody;
+
+
+	void Start () {
+        cachedRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
